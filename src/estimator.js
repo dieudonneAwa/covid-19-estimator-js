2e4f57a821f31a062c40702065c4783359f95289
Awa Dieudonne <dieudonneawa7@gmail.com>
2020-04-08 14:04:28 +0100

    challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -11,29 +11,46 @@ const covid19ImpactEstimator = (data) => {
       impact.infectionsByRequestedTime = impact.currentlyInfected * (
         2 ** Math.floor(((data.timeToElapse * 30) / 3))
       );
+      impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+
       severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (
         2 ** Math.floor(((data.timeToElapse * 30) / 3))
       );
+      severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
       break;
+
     case 'weeks':
       impact.infectionsByRequestedTime = impact.currentlyInfected * (
         2 ** (Math.floor((data.timeToElapse * 7) / 3))
       );
+      impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+
       severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (
         2 ** (Math.floor((data.timeToElapse * 7) / 3))
       );
+      severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
       break;
+
     case 'days':
       impact.infectionsByRequestedTime = impact.currentlyInfected * (
         2 ** (Math.floor((data.timeToElapse * 1) / 3))
       );
+      impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+
       severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (
         2 ** (Math.floor((data.timeToElapse * 1) / 3))
       );
+      severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
       break;
+
     default:
       break;
   }
+
+  impact.hospitalBedsByRequestedTime = data
+    .totalHospitalBeds * (35 / 100) - impact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = data
+    .totalHospitalBeds * (35 / 100) - severeImpact.severeCasesByRequestedTime;
 
   return { data, impact, severeImpact };
 };
